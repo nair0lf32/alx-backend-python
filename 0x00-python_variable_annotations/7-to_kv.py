a01d5a74c470ae66be3a5f33341d624d9365b455
nair0lf32 <florianedem@gmail.com>
2023-01-05 13:25:27 +0100

    do task 7

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+'''
+type annotation function taking a string k and an int OR float v
+then returns a tuple of k, and the square of v as float
+'''
+from typing import Tuple, Union
+
+
+def to_kv(k: str, v: Union[int ,float]) -> Tuple[str, float]:
+    '''returns the sum'''
+    return tuple(k,float(v*v))
