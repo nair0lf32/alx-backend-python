53bd2855eaa99f37100ea96ef8477ea8ca1de218
nair0lf32 <florianedem@gmail.com>
2023-01-05 12:11:32 +0100

    do task 0

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+'''
+type annotation function taking two floats a an b
+then returns their sum as a float
+'''
+
+def add(a: float, b: float) -> float:
+    '''returns the sum'''
+    return a + b
---
+++ 0x00-python_variable_annotations/README.md
@@ -0,0 +1,3 @@
+# Python Variable Annotations
+
+type annotations 
