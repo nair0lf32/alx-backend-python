f86cb425dfd1c6cb3aff01fbfd99a39c04d1246a
nair0lf32 <florianedem@gmail.com>
2023-01-05 12:19:46 +0100

    do task 1

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+'''
+type annotation function taking two strings str1 an str2
+then returns their concatenation result
+'''
+
+
+def add(str1: str, str2: str) -> str:
+    '''returns the sum'''
+    return str1 + str2
