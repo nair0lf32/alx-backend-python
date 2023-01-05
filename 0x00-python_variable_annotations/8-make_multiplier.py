16d5cf7eb8ab240db3ca1c58f8887bb758368ddc
nair0lf32 <florianedem@gmail.com>
2023-01-05 13:59:39 +0100

    do task 8

---
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+'''
+type annotation function taking a float and returns a function that multiply
+a float by the given multiplier
+'''
+from typing import Callable
+
+
+def multiply(a: float, b: float):
+    return a * b
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    '''returns the tuple'''
+    return multiply(multiplier, 0.0)
