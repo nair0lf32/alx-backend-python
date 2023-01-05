fb240742035e04d1866f6eff0431f1cfcef71eed
nair0lf32 <florianedem@gmail.com>
2023-01-05 13:26:46 +0100

    fix task 7

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -6,6 +6,6 @@ then returns a tuple of k, and the square of v as float
 from typing import Tuple, Union
 
 
-def to_kv(k: str, v: Union[int ,float]) -> Tuple[str, float]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     '''returns the sum'''
-    return tuple(k,float(v*v))
+    return tuple(k, float(v*v))
