18d5bd369fd43431624ba6f5e205bd236fcce7d6
nair0lf32 <florianedem@gmail.com>
2023-01-05 12:31:04 +0100

    do task 3

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+'''
+type annotation function taking a float n
+then returns its string representation
+'''
+
+
+def to_str(n: float) -> str:
+    '''returns the str repr'''
+    return str(n)
