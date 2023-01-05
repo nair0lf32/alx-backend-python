49728c9f9cd0a0f22c5d44f5619758113f895237
nair0lf32 <florianedem@gmail.com>
2023-01-05 12:16:31 +0100

    fix task 0

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
+
 '''
 type annotation function taking two floats a an b
 then returns their sum as a float
 '''
+
 
 def add(a: float, b: float) -> float:
     '''returns the sum'''
